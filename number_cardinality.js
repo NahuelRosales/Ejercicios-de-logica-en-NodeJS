e2b17f79043f70ae82b7c192dfad779178bbd6a9
Nahuel Rosales <nahuelrosales@outlook.com>
2023-05-16 21:32:15 -0300

    subo ejercicios

---
+++ number_cardinality.js
@@ -0,0 +1,12 @@
+function number_cardinality(number) {
+    if (number % 10 === 0) {
+      return "zero";
+    } else if (number % 10 === 5) {
+      return "five";
+    } else if (number % 2 === 0) {
+      return "even";
+    } else {
+      return "odd";
+    }
+  }
+  console.log(number_cardinality(1002))
