36efe16fc80625ff162dd6656034090e7bc1342d
Nahuel Rosales <nahuelrosales@outlook.com>
2023-05-16 21:54:25 -0300

    working in range_vision

--- range_vision.js
+++ range_vision.js
@@ -1,11 +1,19 @@
-
 function range_vision(number) {
     if (number < 0) {
-      return false;
+        return false;
     }
-    
-    const digits = String(number).split("").map(Number);
-  
-  }
 
-  
+    const digitos = String(number).split("").map(Number);
+
+    digitos.some((digito, index)=>{
+        const digitosIzquierda = digitos.slice(0,index).reverse()
+        const valorDeVision = digitosIzquierda.reduce((valorAnterior, valorActual) => {
+            return valorAnterior + valorActual;
+          }, 0);
+        console.log(valorDeVision)
+    })
+
+    console.log(digitos);
+}
+
+console.log(range_vision(12305))
